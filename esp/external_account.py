33aec8c6611e7a158dd745aa780f9f87b40b406b
Brad Davidson <brad@oatmail.org>
2017-05-31 16:56:50 -0700

    Apparently Credential needs to be in its own module for loading to work

--- esp/external_account.py
+++ esp/external_account.py
@@ -10,10 +10,3 @@ class ExternalAccount(ESPResource):
         if 'external_id' not in kwargs:
             kwargs['external_id'] = str(uuid.uuid4())
         return super(ExternalAccount, cls).create(**kwargs)
-
-
-# some API responses return the external account namespaced as credential in the
-# relationships field so I'm creating this hack to navigate around that issue.
-class Credential(ExternalAccount):
-
-    resource_type = 'external_accounts'
